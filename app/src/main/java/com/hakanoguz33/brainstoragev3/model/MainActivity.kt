5b296f780ce76fb32b06027caddd51f4f711ffa8
Hakan Oğuz <hakanoguz33@yandex.com>
2022-04-25 22:11:32 +0300

    Initial commit

---
+++ app/src/main/java/com/hakanoguz33/brainstoragev3/model/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hakanoguz33.brainstoragev3.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hakanoguz33.brainstoragev3.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
